24b6ad63185fee3414c255fb1344636884386b31
Nikhil Mishra <nkmishra1997@gmail.com>
2017-04-24 02:02:00 +0530

    auth.js

---
+++ config/auth.js
@@ -0,0 +1,26 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : '114706539031197', // your App ID
+        'clientSecret'    : '8d20971b0312ff3e117c8ebf93cbd9c6', // your App Secret
+        'callbackURL'     : 'http://localhost:3000/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : 'your-consumer-key-here',
+        'consumerSecret'     : 'your-client-secret-here',
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'         : 'your-secret-clientID-here',
+        'clientSecret'     : 'your-client-secret-here',
+        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
